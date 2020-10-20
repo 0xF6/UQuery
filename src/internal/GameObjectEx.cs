de7fafd03b56f8d39670cc1ef3faec765e973735
Yuuki Wesp <ls-micro@ya.ru>
2020-10-20 07:54:32 +0300

    add GameObject.Child

---
+++ src/internal/GameObjectEx.cs
@@ -0,0 +1,18 @@
+﻿namespace UnityEngine.Query
+{
+    #if !UNITY_LINQ
+    /// <summary>
+    /// LINQ-to-GameObject-for-Unity
+    /// </summary>
+    public static class GameObjectEx
+    {
+        public static GameObject Child(this GameObject origin, string name)
+        {
+            if ((UnityEngine.Object) origin == (UnityEngine.Object) null)
+                return (GameObject) null;
+            Transform transform = origin.transform.Find(name);
+            return (UnityEngine.Object) transform == (UnityEngine.Object) null ? (GameObject) null : transform.gameObject;
+        }
+    }
+    #endif
+}
