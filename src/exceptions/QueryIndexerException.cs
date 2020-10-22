209bf329573d855722a4425778b43bde6e6b1351
Yuuki Wesp <ls-micro@ya.ru>
2020-10-22 03:40:47 +0300

    add QueryIndexerException

---
+++ src/exceptions/QueryIndexerException.cs
@@ -0,0 +1,9 @@
+﻿namespace UnityEngine.Query
+{
+    public class QueryIndexerException : QueryException
+    {
+        public QueryIndexerException(string key, string rawKey, int index, int childLen, string path) : 
+            base($"The indexer '{key}' is not valid. Index: '{index}', childrens size (with '{rawKey}' name): '{childLen}'", path)
+        { }
+    }
+}
